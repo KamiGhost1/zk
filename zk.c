dc85927d187a95a7a491dd74765c774be35ed39e
kamighost1 <romires2000@gmail.com>
2021-12-08 18:55:45 +0300

    work

--- zk.c
+++ zk.c
@@ -1,5 +1,5 @@
 #include "zk.h"
-#define CICLES_CHECK 20
+
 
 void opener(){
     printf("\n\t__    __      ______ \n");
@@ -26,8 +26,15 @@ int check_parameters(int c, char **v){
         }
         if(!strcmp(v[1], "--rand")){
             for(int i = 0; i < 10; i ++){
-                printf("%d\n",get_rand(0,20));
+                printf("%d\n",get_rand(0,1));
             }
+            return 0;
+        }
+        if(!strcmp(v[1], "--nod")){
+            printf("nod (36, 17) = %d\n",NOD(36,17));
+            printf("nod (12, 6) = %d\n",NOD(12,6));
+            printf("nod (1024, 511) = %d\n",NOD(1024,511));
+            printf("nod (22751, 36387) = %d\n",NOD(22751, 36387));
             return 0;
         }
         if(!strcmp(v[1], "--start")){
@@ -47,20 +54,89 @@ int start_zk(int c,char **v){
     return -1;
 }
 
-int zk_mode(int num, int mode){
-    rand();
-    return 0;
+int zk_mode(int num, int mod){
+    if(num < 0){
+        num = mod - (num*-1)%mod;
+    }
+    return num % mod;
 }
 
 int zk_pow(int base, int e){
-    return 0;
+    int num = 1;
+    while( e != 0 ){
+        if( e & 1 ){
+            num *= base;
+        }
+        base *= base;
+        e = e>>1;
+    }
+    return num;
+}
+
+int zk_pow_mode(int base, int e,  int mod){
+    int num = 1;
+    while( e != 0 ){
+        if( e & 1 ){
+            num = num * base % mod;
+        }
+        base = base * base % mod;
+        e = e>>1;
+    }
+    return num;
 }
 
 void zk(){
     printf("i'm started!\n");
+    int N = P * Q;
+    int secretA = N;
+    while(NOD(secretA,N) != 1){
+        secretA = get_rand(1, N-1);
+    }
+    int r ,publicA, x, e, y , yroot, ysqrt;
+    int VALID = 0;
+    publicA = zk_pow_mode(secretA,2,N);
+    #ifdef DEBUG_MAIN
+    printf("secret A: %d\npublic A: %d\nN: %d\n", secretA, publicA, N);
+    #endif
+    for(int i = 0; i < CICLES_CHECK; i++){
+        r = get_rand(1, N-1);
+        x = zk_pow_mode(r, 2, N);
+        e = get_rand(0,1);
+        #ifdef CHECK_SECRET_VALID
+        secretA-=1;
+        #endif
+        y = zk_pow_mode(r*secretA, e, N);
+        yroot = zk_pow_mode(x*publicA, e, N);
+        ysqrt = zk_pow_mode(y,2,N);
+
+        #ifdef DEBUG_CICLE
+        printf("i: %d;\ty==yroot ?  %s\tysqrt: %d\tyroot: %d\n", i+1, ysqrt==yroot ? "true" : "false", ysqrt, yroot);
+                #ifdef DEBUG_ALL
+                printf("\nr: %d\te: %d\tx: %d\n\n",r, e, x);
+                #endif
+        #endif
+        if(yroot == ysqrt)
+            VALID++;
+    }
+    if(VALID != CICLES_CHECK){
+        printf("Not Valid. Access denied!\n");
+    }else{
+        printf("Valid. Access allowed!\n");
+    }
     
 }
 
 int get_rand(const int min, const int max) {
     return rand() % (max - min + 1) + min;
+}
+
+int NOD(int a, int b){
+    while(a > 0 && b > 0)
+
+        if(a > b)
+            a %= b;
+        else
+            b %= a;
+ 
+    return a + b;
 }
--- zk.h
+++ zk.h
@@ -1,11 +1,20 @@
 #include <string.h>
 #include <stdio.h>
 #include <time.h>
+#define CICLES_CHECK 20
+#define P 311
+#define Q 117
+#define DEBUG_MAIN
+#define DEBUG_CICLE
+// #define DEBUG_ALL
+// #define CHECK_SECRET_VALID
 
 void opener();
 int check_parameters(int c, char **v);
 int start_zk(int c,char **v);
-int zk_mode(int num, int mode);
+int zk_mode(int num, int mod);
 int zk_pow(int base, int e);
+int zk_pow_mode(int base, int e, int mod);
+int NOD(int a, int b);
 void zk();
 int get_rand(const int min, const int max);
